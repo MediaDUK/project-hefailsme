c869591362a734060a1c3d94b410a5e553160a2e
MediaDUK <hello@mediaduk.com>
2018-05-03 20:13:48 -0700

    added bs4

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require('path')
 const hb = require('express-handlebars');
 const PORT = process.env.PORT || 5000
 
+app.use(express.static(__dirname+'/public'))
+
+
 app.engine('.hbs', hb({ defaultLayout: 'main', extname: '.hbs' }))
   .set('view engine', '.hbs')
   .get('/',  (req, res) => {
