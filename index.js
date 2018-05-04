cdfabcc5a457979ea4738ee2a6e566a60d98f820
MediaDUK <hello@mediaduk.com>
2018-05-03 20:00:01 -0700

    setting up heroku dev still

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const hb = require('express-handlebars');
+const PORT = process.env.PORT || 5000
+
+app.engine('.hbs', hb({ defaultLayout: 'main', extname: '.hbs' }))
+  .set('view engine', '.hbs')
+  .get('/',  (req, res) => {
+    res.render('home')
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
