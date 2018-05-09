d2cb664c6595d9b7fbeb0eece9770d6dbdb0ca4b
MediaDUK <hello@mediaduk.com>
2018-05-08 19:54:59 -0700

    removed video from repo

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,49 @@
-console.log('hello')
+$(document).ready(function() {
+
+  // popup window on homepage
+  function startPopup(){
+    var time = 5000
+    setTimeout(function () {
+      var $EmailModal = $('#emailSubscription')
+      EmailModal.modal('show')
+    }, time);
+  }
+  startPopup()
+
+  // posting the data to JAW_DB on heroku
+ function postEmail(email_to_post) {
+    console.log('Posting to database: ' + email_to_post)
+    // create ajax object
+    var request = $.ajax({
+      url: '/email-subscription',
+      method: "POST",
+      data: {
+        email: email_to_post
+      },
+      dataType: "html"
+    });
+
+    // handle success
+    request.done(function (msg) {
+      console.log('Message from post response: '+msg)
+      // close modal after success from jawsbd posts
+      var $EmailModal = $('#emailSubscription')
+      EmailModal.modal('hide')
+    });
+
+    // handle error
+    request.fail(function (jqXHR, textStatus) {
+      alert("Request failed: " + textStatus);
+    });
+  }
+
+  // click event for email form / button
+  $('#subscriptionButton').on('click', function(event) {
+    event.preventDefault()
+    var $email = $('#email')
+    var emailData = $email.val().trim() // string
+    postEmail(emailData)
+  });
+
+
+})
