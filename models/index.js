a8b02c5a773be0c8944bfe7c9db9f88ce65b57a7
Roger Camero <rogercamero@icloud.com>
2018-07-21 11:20:35 -0700

    heroku fixed and cleaning up files

--- models/index.js
+++ models/index.js
@@ -15,22 +15,9 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   console.log('CONNECTING TO LOCAL')
-  // var sequelize = new Sequelize(process.env.JAWSDB_DATABASE_NAME, 
-  //                               process.env.JAWSDB_USERNAME, process.env.JAWSDB_PASSWORD, {
-  //   host: '127.0.0.1',
-  //   port: process.env.JAWSDB_PORT,
-  //   dialect: 'mysql'
-  // });
-  // var sequelize = new Sequelize('jgy2mx5qwhjczyqk', 'lv2n8lbwqo7dqjok', 'azqw5bjnl3ukmgbs', {
-  //   host: 'localhost',
-  //   port: process.env.JAWSDB_PORT,
-  //   dialect: 'mysql',
-  // });
-
-
 }
 
-var sequelize = new Sequelize('mysql://lv2n8lbwqo7dqjok:drl5hm5kwxb9ikz7@i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jgy2mx5qwhjczyqk');
+// var sequelize = new Sequelize('mysql://lv2n8lbwqo7dqjok:drl5hm5kwxb9ikz7@i943okdfa47xqzpy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jgy2mx5qwhjczyqk');
 
 
 
